515bc10a9793c87b40ad665e6365f31363fddc57
Nguyễn Kim Kha <nkimkha@gmail.com>
2016-02-28 20:29:53 +0700

    Update

--- app/src/main/java/org/moire/opensudoku/gui/GameSettingsActivity.java
+++ app/src/main/java/org/moire/opensudoku/gui/GameSettingsActivity.java
@@ -21,12 +21,32 @@
 package org.moire.opensudoku.gui;
 
 import org.moire.opensudoku.R;
+import org.moire.opensudoku.gui.ext.AppCompatPreferenceActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class GameSettingsActivity extends PreferenceActivity {
+public class GameSettingsActivity extends AppCompatPreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,6 +55,113 @@ public class GameSettingsActivity extends PreferenceActivity {
 
 		findPreference("show_hints").setOnPreferenceChangeListener(mShowHintsChanged);
 	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+        setUpScreen(null, null);
+	}
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        super.onPreferenceTreeClick(preferenceScreen, preference);
+
+        // If the user has clicked on a preference screen, set up the screen
+        if (preference instanceof PreferenceScreen) {
+            preferenceScreen = (PreferenceScreen) preference;
+            setUpScreen(preferenceScreen.getDialog(), preferenceScreen.getTitle());
+        }
+
+        return false;
+    }
+
+    public void setUpScreen(@Nullable final Dialog dialog, @Nullable final CharSequence title) {
+        Toolbar bar;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            LinearLayout root;
+            if (dialog == null) {
+                root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+            } else {
+                root = (LinearLayout) dialog.findViewById(android.R.id.list).getParent();
+            }
+            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar, root, false);
+            root.addView(bar, 0); // insert at top
+        } else {
+            ViewGroup root;
+            if (dialog == null) {
+                root = (ViewGroup) findViewById(android.R.id.content);
+            } else {
+                root = (ViewGroup) dialog.findViewById(android.R.id.content);
+            }
+            ListView content = (ListView) root.getChildAt(0);
+
+            root.removeAllViews();
+
+            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar, root, false);
+
+            int height;
+            TypedValue tv = new TypedValue();
+            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+                height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+            }else{
+                height = bar.getHeight();
+            }
+
+            content.setPadding(0, height, 0, 0);
+
+            root.addView(content);
+            root.addView(bar);
+        }
+
+        if (title != null) {
+            bar.setTitle(title);
+        }
+
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog == null) {
+                    finish();
+                } else {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        if (dialog == null) {
+            setSupportActionBar(bar);
+        }
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        // Allow super to try and create a view first
+        final View result = super.onCreateView(name, context, attrs);
+        if (result != null) {
+            return result;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            // If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+            // standard framework versions
+            switch (name) {
+                case "EditText":
+                    return new AppCompatEditText(this, attrs);
+                case "Spinner":
+                    return new AppCompatSpinner(this, attrs);
+                case "CheckBox":
+                    return new AppCompatCheckBox(this, attrs);
+                case "RadioButton":
+                    return new AppCompatRadioButton(this, attrs);
+                case "CheckedTextView":
+                    return new AppCompatCheckedTextView(this, attrs);
+            }
+        }
+
+        return null;
+    }
 
 	private OnPreferenceChangeListener mShowHintsChanged = new OnPreferenceChangeListener() {
 
